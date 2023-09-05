8e415ee169075e46d411e5ad2f2b0a8887ef66af
BlaChalk <black00731@gmail.com>
2023-09-05 23:58:57 +0800

    feat: 1. 新增客製化字體與調整字體大小 2. banner 與 section1 畫面調整

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        PtypeDisplay: 'PtypeDisplay, sans-serif',
+        PtypeText: 'PtypeText, sans-serif',
+        Quakerhack: 'Quakerhack, sans-serif',
+      },
+      fontSize: {
+        '6xl': ['16rem', {
+          lineHeight: '16rem',
+        }],
+        '9xl': ['24rem', {
+          lineHeight: '20rem',
+        }],
+        '12xl': ['48rem', {
+          lineHeight: '40rem',
+        }],
+      },
     },
   },
   plugins: [],
